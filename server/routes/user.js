fa2a5c8434502d28664974e9ca33c5c6fea1e7bd
McKay Stevenson <mckay800@gmail.com>
2021-03-16 23:45:05 -0400

    payment

--- server/routes/user.js
+++ server/routes/user.js
@@ -6,15 +6,13 @@ const router = express.Router();
 const { authCheck } = require('../middlewares/auth');
 
 // controller
-const {userCart, getUserCart, emptyCart, saveAddress } = require('../controllers/user');
+const {userCart, getUserCart, emptyCart, saveAddress, applyCouponToUserCart } = require('../controllers/user');
 
 router.post('/user/cart', authCheck, userCart); // save cart
 router.get('/user/cart', authCheck, getUserCart); // get cart
 router.delete('/user/cart', authCheck, emptyCart);  // empty cart
 router.post('/user/address', authCheck, saveAddress);
-
-
-
+router.post('/user/cart/coupon', authCheck, applyCouponToUserCart);  // coupon
 
 // below was temporary
 // router.get('/user', (req, res) => {
